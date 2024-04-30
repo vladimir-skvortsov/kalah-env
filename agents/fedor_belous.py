08c25c3f80794740905cf004efc81c9b31a91fd9
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2024-04-30 20:18:39 +0700

    Add test bots

--- agents/fedor_belous.py
+++ agents/fedor_belous.py
@@ -2,4 +2,4 @@ import random
 
 def get_move(state):
   valid_moves = [i for i, house in enumerate(state) if house > 0 and 0 <= i <= 5]
-  return random.choice(valid_moves)
+  return valid_moves[-1]
